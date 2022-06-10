abe348ce7053d4362ecdb67ad69e08d7551ee211
Felix Dominguez <66543823+StickyFingers84@users.noreply.github.com>
2022-06-10 13:36:33 -0600

    Segundo Commit
    Main simple

--- P3_Lab8_FelixDominguez.cpp
+++ P3_Lab8_FelixDominguez.cpp
@@ -4,6 +4,38 @@ using namespace std;
 
 int main()
 {
-    
+    int ciclo = 1;
+    while (ciclo == 1) {
+        cout << "1. Login" << endl;
+        cout << "2. Salir" << endl;
+        cout << endl;
+        cout << "Ingrese el numero de la opcion que desea realizar: ";
+        int opcion;
+        cin >> opcion;
+        cout << endl;
+        switch (opcion) {
+            case 1: {
+                cout << "Ingrese el nombre de usuario: ";
+                string nombre;
+                cin >> nombre;
+                cout << "Ingrese la contrasenia: ";
+                string contrasenia;
+                cin >> contrasenia;
+                cout << endl;
+                if (nombre == "admin" && contrasenia == "perfectMarket") {
+                    cout << "Entrastes" << endl;
+                }
+                else {
+                    cout << "No ingreso el nombre o contrasenia correctos" << endl;
+                }
+                cout << endl;
+                break;
+            }
+            case 2: {
+                ciclo = 0;
+                break;
+            }
+        }
+    }
 }
 
